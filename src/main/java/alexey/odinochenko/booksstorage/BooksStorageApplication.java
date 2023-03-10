c0a675ddd4a2869ea92cab7ead7b09717535e1f1
Alexey Odinochenko <conik9999@mail.ru>
2023-03-10 13:04:49 +0300

    init: init repo

---
+++ src/main/java/alexey/odinochenko/booksstorage/BooksStorageApplication.java
@@ -0,0 +1,12 @@
+package alexey.odinochenko.booksstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksStorageApplication.class, args);
+    }
+}
---
+++ src/test/java/alexey/odinochenko/booksstorage/BooksStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package alexey.odinochenko.booksstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
